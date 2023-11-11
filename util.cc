55d1dfb3e7ecd1bbee49db0cfcc6ae73f18151b8
Benlin Gan <benlingan@gmail.com>
2023-11-11 17:12:15 -0600

    finish coding?

--- util.cc
+++ util.cc
@@ -3,6 +3,9 @@
 #include <stack>
 #include <vector>
 #include <iostream>
+#include <random>
+#include <cstdlib>
+#include <chrono>
 bool is_balanced(std::string s){
 	std::stack<char> st;
 	for(char c: s){
@@ -38,6 +41,7 @@ unsigned brute_force(std::string s){
 }
 unsigned scan_left(std::string& s, unsigned index){
 	std::stack<char> st;
+	unsigned fin = index;
 	for(int start = index; start >= 0; start--){
 		char c = s[start];
 		if(c == ')' || c == ']'){
@@ -45,18 +49,20 @@ unsigned scan_left(std::string& s, unsigned index){
 		}else if(c == '('){
 			if(!st.empty() && st.top() == ')'){
 				st.pop();
+				if(st.empty()) fin = start;
 			}else{
-				return index - start - st.size(); 
+				return index - fin + 1; 
 			}
 		}else if(c == '['){
 			if(!st.empty() && st.top() == ']'){
 				st.pop();
+				if(st.empty()) fin = start;
 			}else{
-				return index - start - st.size();
+				return index - fin + 1;
 			}
 		}
 	}
-	return index + 1 - st.size(); 
+	return index - fin + 1; 
 } 
 unsigned scan_left(std::string s){
 	unsigned max_size = 0;
@@ -77,6 +83,10 @@ unsigned basic_recursion(std::string& s, unsigned index){
 		if(s[index - 1] == '[') return basic_recursion(s, index - 2) + 2;
 		if(s[index - 1] != '('){
 			unsigned prev_length = basic_recursion(s, index - 1);			
+			if(index == prev_length + 1){
+				return (s[0] == '[') ? prev_length + 2 : 0;
+			}
+			if(index == prev_length) return 0;
 			char toMatch = s[index - prev_length - 1];
 			if(toMatch == '['){
 				return basic_recursion(s, index - prev_length - 2) + prev_length + 2;
@@ -88,6 +98,10 @@ unsigned basic_recursion(std::string& s, unsigned index){
 		if(s[index - 1] == '(') return basic_recursion(s, index - 2) + 2;
 		if(s[index - 1] != '['){
 			unsigned prev_length = basic_recursion(s, index - 1);			
+			if(index == prev_length + 1){
+				return (s[0] == '(') ? prev_length + 2 : 0;
+			}
+			if(index == prev_length) return 0;
 			char toMatch = s[index - prev_length - 1];
 			if(toMatch == '('){
 				return basic_recursion(s, index - prev_length - 2) + prev_length + 2;
@@ -123,6 +137,13 @@ void dynamic_programming(std::string &s, std::vector<unsigned>& max_lengths){
 			}
 			if(s[i - 1] != '('){
 				unsigned prev_length = max_lengths[i - 1];
+				if(i == prev_length + 1){
+					max_lengths[i] = (s[0] == '[') ? prev_length + 2 : 0;
+					continue;
+				}
+				if(i == prev_length){
+					max_lengths[i] = 0; continue;
+				}
 				char toMatch = s[i - prev_length - 1];  
 				if(toMatch == '['){
 					max_lengths[i] = max_lengths[i - prev_length - 2] + prev_length + 2; 
@@ -137,6 +158,13 @@ void dynamic_programming(std::string &s, std::vector<unsigned>& max_lengths){
 			}
 			if(s[i - 1] != '['){
 				unsigned prev_length = max_lengths[i - 1];
+				if(i == prev_length + 1){
+					max_lengths[i] = (s[0] == '(') ? prev_length + 2 : 0;
+					continue;
+				}
+				if(i == prev_length){
+					max_lengths[i] = 0; continue;
+				}
 				char toMatch = s[i - prev_length - 1];  
 				if(toMatch == '('){
 					max_lengths[i] = max_lengths[i - prev_length - 2] + prev_length + 2; 
@@ -156,10 +184,50 @@ unsigned dynamic_programming(std::string s){
 	}
 	return max_size;
 }
-int main(){
-	std::string s = "[][][][][][][(()(]()[()()](])([]()()()()()(]]";
-	std::cout << brute_force(s) << '\n';
-	std::cout << scan_left(s) << '\n';
-	std::cout << basic_recursion(s) << '\n';
-	std::cout << dynamic_programming(s) << '\n';
+
+
+
+char random_bracket(std::mt19937& gen){
+	std::uniform_int_distribution<> distrib(0, 3);
+	char v[4] = {'(', '[', ']', ')'};
+	return v[distrib(gen)];
+}
+int main(int argc, char** argv){
+	std::mt19937 gen;
+	if(argc != 2){
+		std::cout << "Need exactly 2 arguments";
+		return 1;
+	}
+	int n = std::atoi(argv[1]);
+	std::vector<char> v(n);
+	for(int i = 0; i < n; i++){
+		v[i] = random_bracket(gen);
+	}
+    auto start{std::chrono::steady_clock::now()};
+   	auto end{std::chrono::steady_clock::now()};
+	std::string s(v.begin(), v.end());
+	//brute force
+	/*start = std::chrono::steady_clock::now();
+	std::cout << brute_force(s);
+	end = std::chrono::steady_clock::now();
+	std::chrono::duration<double> bf{end - start};
+	std::cout << ' ' << bf.count() << '\n';*/
+	//scan_left
+	start = std::chrono::steady_clock::now();
+	std::cout << scan_left(s);
+	end = std::chrono::steady_clock::now();
+	std::chrono::duration<double> sl{end - start};
+	std::cout << ' ' << sl.count() << '\n';
+	//basic_recursion
+	start = std::chrono::steady_clock::now();
+	std::cout << basic_recursion(s);
+	end = std::chrono::steady_clock::now();
+	std::chrono::duration<double> br{end - start};
+	std::cout << ' ' << br.count() << '\n';
+	//dp
+	start = std::chrono::steady_clock::now();
+	std::cout << dynamic_programming(s);
+	end = std::chrono::steady_clock::now();
+	std::chrono::duration<double> dp{end - start};
+	std::cout << ' ' << dp.count() << '\n';
 }
